40c9b041533cd514dd3853285d085abd6048e7b0
abderrahmannjeh <njeh.abderrahman@gmail.com>
2022-01-03 21:57:27 +0100

    update startup

--- VirtualMallBackEnd/Startup.cs
+++ VirtualMallBackEnd/Startup.cs
@@ -1,16 +1,24 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VirtualMallBackEnd.Context;
+using VirtualMallBackEnd.Entity;
+using VirtualMallBackEnd.Service.Authontification;
+using VirtualMallBackEnd.Service.Authontification.IAuthebtification;
 
 namespace VirtualMallBackEnd
 {
@@ -32,6 +40,36 @@ namespace VirtualMallBackEnd
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "VirtualMallBackEnd", Version = "v1" });
             });
+            //services.AddLocalization(o=>o.ResourcesPath= "Resources");
+            services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MyDataBase")));
+            services.AddIdentity<Account, IdentityRole>()
+                    .AddEntityFrameworkStores<AppDbContext>()
+                    .AddDefaultTokenProviders();
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+
+
+            }).AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = Configuration["JWT:ValidAudience"],
+                    ValidIssuer = Configuration["JWT:ValidIssuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(Configuration["JWT:key"]))
+
+
+                };
+
+
+            });
+            services.AddScoped<IAuthentificationService, AuthentificationService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
